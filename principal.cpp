735c35f030ff4c01a359e5aec9632ad6740aa880
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-12 08:08:36 -0600

    Se agrego el archivo de menu

--- principal.cpp
+++ principal.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "MisArchivos\\funciones.cpp"
+#include "MisArchivos\\menu.cpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "/tMi Proyecto - Prueba" << endl;
+    cout << "\tMi Proyecto - Prueba" << endl;
+    bienvenida();
+    
     return 0;
 }
